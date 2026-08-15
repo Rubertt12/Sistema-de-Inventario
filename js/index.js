d4500e3785a07a9a5c7a224dd84e9d862d439ea7
Rúbertt Ramires <54216027+Rubertt12@users.noreply.github.com>
2026-08-15 00:09:07 -0300

    feat: load machine location map module

--- js/index.js
+++ js/index.js
@@ -138,6 +138,7 @@
       await load('/js/scanner.js');
       await load('/js/transfer-v2.js');
       await load('/js/machine-details-v2.js');
+      await load('/js/machine-location-map.js?v=20260815-1');
       await load('/js/ticket-author-bridge.js');
       await load('/js/asset-history.js');
       await load('/js/dashboard-hotfix.js');
