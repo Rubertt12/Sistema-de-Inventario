b6de417a677099624c4dec7357b61a530600e82d
Rúbertt Ramires <54216027+Rubertt12@users.noreply.github.com>
2026-08-14 23:24:07 -0300

    feat: load company registration module in platform admin

--- js/admin-login-preview-fix.js
+++ js/admin-login-preview-fix.js
@@ -32,6 +32,7 @@
     await load('/js/footer-v2.js');
     await load('/js/admin-branding-overlay-v2.js');
     await load('/js/admin-company-ux-v2.js');
+    await load('/js/admin-company-create.js?v=20260814-1');
     await load('/js/admin-registration-approval.js');
     await load('/js/support-admin-permissions.js');
     await load('/js/collaborator-management.js');
